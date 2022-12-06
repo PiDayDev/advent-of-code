8931e0ba192a712209646574ce9dc9f1a9dddaf5
Damiano Salvi <salvi@intre.it>
2022-12-06 09:08:11 +0100

    ✨ 2022 🎅 Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,21 @@
+private const val DAY = "06"
+
+fun main() {
+    fun String.hasRepetitions() = toSet().size < length
+
+    fun detectMarkerPacket(stream: String, length: Int): Int {
+        val lengthOfInvalidPrefix = stream
+            .windowed(length)
+            .takeWhile { it.hasRepetitions() }
+            .size
+        return lengthOfInvalidPrefix + length
+    }
+
+    fun part1(input: String) = detectMarkerPacket(input, 4)
+
+    fun part2(input: String) = detectMarkerPacket(input, 14)
+
+    val input = readInput("Day${DAY}").first()
+    println(part1(input))
+    println(part2(input))
+}
