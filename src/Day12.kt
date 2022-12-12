d82c13c4150fbf1dde817750ba697af0ca972ee4
Damiano Salvi <salvi@intre.it>
2022-12-12 18:03:35 +0100

    ✨ 2022 🎅 Day 12

---
+++ src/Day12.kt
@@ -0,0 +1,66 @@
+private const val DAY = 12
+
+private typealias Location = Pair<Int, Int>
+
+private operator fun List<String>.get(xy: Location): Char = this[xy.second][xy.first]
+
+private fun List<String>.altitude(xy: Location) =
+    when (val cell = this[xy]) {
+        'S' -> 'a'
+        'E' -> 'z'
+        else -> cell
+    }.code
+
+private fun List<String>.destinations(xy: Location): List<Location> {
+    val z = altitude(xy)
+    val (x, y) = xy
+    return listOf(x + 1 to y, x - 1 to y, x to y + 1, x to y - 1)
+        .filter { (x, y) -> x in first().indices && y in indices }
+        .filter { altitude(it) <= z + 1 }
+}
+
+
+fun main() {
+
+    fun solveFor(input: List<String>, x0: Int, y0: Int, maxLimit: Int = 99999): Int? {
+        val yF = input.indexOfFirst { "E" in it }
+        val xF = input[yF].indexOf("E")
+        val h = input.size
+        val w = input.first().length
+
+        val costs = mutableMapOf((x0 to y0) to 0)
+
+        var currentCost = 0
+        var oldSize = 0
+        while (costs.size in oldSize + 1 until w * h && currentCost <= maxLimit) {
+            oldSize = costs.size
+            val edge = costs.keys.filter { costs[it] == currentCost }
+            currentCost++
+            edge.flatMap { input.destinations(it) }
+                .forEach { costs.putIfAbsent(it, currentCost) }
+        }
+
+        return costs[xF to yF]
+    }
+
+    fun part1(input: List<String>): Int {
+        val y0 = input.indexOfFirst { "S" in it }
+        val x0 = input[y0].indexOf("S")
+        return solveFor(input, x0, y0)!!
+    }
+
+    fun part2(input: List<String>) =
+        input.mapIndexedNotNull { y, row ->
+            row.mapIndexedNotNull { x, c ->
+                if (c == 'a') {
+                    solveFor(input, x, y)
+                } else
+                    null
+            }.minOrNull()
+        }.minOrNull()!!
+
+    val input = readInput("Day${DAY}")
+    println(part1(input))
+    println(part2(input))
+
+}
