0a52aad37cae5d03786979075af03feb1dc80346
Damiano Salvi <salvi@intre.it>
2022-12-17 14:09:41 +0100

    ✨ 2022 🎅 Day 15

--- src/RangeUtils.kt
+++ src/RangeUtils.kt
@@ -17,17 +17,40 @@ infix fun <T : Comparable<T>> ClosedRange<T>.union(other: ClosedRange<T>): List<
         else -> listOf(this, other).sortedBy { it.start }
     }
 
-fun <T : Comparable<T>> List<ClosedRange<T>>.union(): List<ClosedRange<T>> {
+infix fun <T : Comparable<T>> ClosedRange<T>.intersection(other: ClosedRange<T>): List<ClosedRange<T>> =
+    when {
+        this in other -> listOf(this)
+        other in this -> listOf(other)
+        this overlaps other -> {
+            val min = if (start > other.start) start else other.start
+            val max = if (endInclusive < other.endInclusive) endInclusive else other.endInclusive
+            listOf(min..max)
+        }
+
+        else -> emptyList()
+    }
+
+fun <T : Comparable<T>> List<ClosedRange<T>>.union() =
+    reduceWith(ClosedRange<T>::union)
+
+fun <T : Comparable<T>> List<ClosedRange<T>>.intersection() =
+    reduceWith(ClosedRange<T>::intersection)
+
+private fun <T : Comparable<T>> List<ClosedRange<T>>.reduceWith(
+    function: ClosedRange<T>.(ClosedRange<T>) -> List<ClosedRange<T>>
+): List<ClosedRange<T>> {
     var k = this
-    var z = k.unionStep()
+    var z = k.recursionStep(function)
     while (z.size != k.size) {
         k = z
-        z = k.unionStep()
+        z = k.recursionStep(function)
     }
     return z
 }
 
-private fun <T : Comparable<T>> List<ClosedRange<T>>.unionStep(): List<ClosedRange<T>> =
+private fun <T : Comparable<T>> List<ClosedRange<T>>.recursionStep(
+    function: ClosedRange<T>.(ClosedRange<T>) -> List<ClosedRange<T>>
+) =
     drop(1).fold(take(1)) { acc: List<ClosedRange<T>>, curr: ClosedRange<T> ->
-        acc.flatMap { it union curr }.distinct()
+        acc.flatMap { it.function(curr) }.distinct()
     }
