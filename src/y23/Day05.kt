cbde778be82118e647a0ec5cd224d9ed09f0cb19
Damiano Salvi <salvi@intre.it>
2023-12-05 14:03:27 +0100

    ✨ 2023 ⚡ Day 5 - performance

--- src/y23/Day05.kt
+++ src/y23/Day05.kt
@@ -3,47 +3,35 @@ package y23
 private const val DAY = "05"
 
 fun main() {
-    fun List<String>.longs() =
+    fun List<String>.seeds() =
         first().substringAfter(": ").split(" ").map { it.toLong() }
 
     fun part1(input: List<String>): Long {
-        val seeds = input.longs()
+        val seeds = input.seeds()
         val sections = input.drop(2).sections()
 
         var result = seeds
         sections.forEach { section ->
             val mapper = Mapper(section)
-            result = result.map { mapper.map(it) }
+            result = result.map { mapper.convert(it) }
         }
 
         return result.minOf { it }
     }
 
-    /**
-     * ⚠ Warning: terrible performances
-     */
     fun part2(input: List<String>): Long {
-        val seeds: Sequence<Long> = input.longs()
-            .chunked(2)
-            .map { (a,b)-> (a until a+b).asSequence() }
-            .asSequence()
-            .flatten()
         val sections = input.drop(2).sections()
         val mappers = sections.map { Mapper(it) }
 
-        val (m1,m2,m3,m4,m5) = mappers
-        val (m6,m7) = mappers.drop(5)
+        val seedsRanges: List<LongRange> = input.seeds()
+            .chunked(2)
+            .map { (a, b) -> a until a + b }
 
-        val min=seeds
-            .map { m1.map(it) }
-            .map { m2.map(it) }
-            .map { m3.map(it) }
-            .map { m4.map(it) }
-            .map { m5.map(it) }
-            .map { m6.map(it) }
-            .map { m7.map(it) }
-            .minOf { it }
-        return min
+        val convertedRanges: List<LongRange> = mappers
+            .fold(seedsRanges) { ranges, mapper ->
+                ranges.flatMap { mapper.convertRange(it) }
+            }
+        return convertedRanges.minOf { it.first }
     }
 
     val input = readInput("Day$DAY")
@@ -62,23 +50,60 @@ fun List<String>.sections(): List<List<String>> =
 
     }
 
-data class Mapper(val section: List<String>) {
-    val id = section.first()
+private data class Mapper(val section: List<String>) {
     private val rangeToIncrement = mutableMapOf<LongRange, Long>()
 
     init {
-        val mappings = section.drop(1)
-        mappings.forEach { row ->
-            val (target, origin, length) = row.split(" ").map { it.toLong() }
-            val originRange = origin until origin + length
-            val difference = target - origin
-            rangeToIncrement[originRange] = difference
-        }
+        section
+            .drop(1)
+            .forEach { row ->
+                val (target, origin, length) = row.split(" ").map { it.toLong() }
+                val originRange = origin until origin + length
+                val difference = target - origin
+                rangeToIncrement[originRange] = difference
+            }
     }
 
-    fun map(n: Long): Long {
+    fun convert(n: Long): Long {
         val increment = rangeToIncrement.entries
             .firstNotNullOfOrNull { (range, diff) -> if (n in range) diff else null }
         return (increment ?: 0L) + n
+    }
+
+    fun convertRange(range: LongRange): List<LongRange> = split(range)
+        .map { r -> convert(r.first)..convert(r.last) }
+
+    private fun split(range: LongRange): List<LongRange> {
+        val limits = rangeToIncrement.keys
+            .map { r -> range.intersect(r) }
+            .filterNot { it.isEmpty() }
+            .sortedBy { it.first }
+
+        if (limits.isEmpty())
+            return listOf(range)
+
+        val edges = listOf(
+            range.first until limits.first().first,
+            limits.last().last + 1..range.last,
+        )
+        val holes = limits.windowed(2).map { (left, right) ->
+            left.last + 1 until right.first
+        }
+
+        return (limits + edges + holes).filterNot { it.isEmpty() }
+    }
+}
+
+private fun LongRange.intersect(other: LongRange): LongRange {
+    val bFirst = other.first
+    val bLast = other.last
+    return when {
+        bFirst > last -> LongRange.EMPTY
+        bLast < first -> LongRange.EMPTY
+        bFirst in this && bLast in this -> other
+        first in other && last in other -> this
+        bFirst in this -> bFirst..last
+        bLast in this -> first..bLast
+        else -> LongRange.EMPTY
     }
 }
