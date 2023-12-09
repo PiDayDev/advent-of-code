051506546fb5e31e6931208b28cba674db18f641
Damiano Salvi <salvi@intre.it>
2023-12-09 08:23:35 +0100

    ✨ 2023 🎅 Day 9

---
+++ src/y23/Day09.kt
@@ -0,0 +1,24 @@
+package y23
+
+private const val DAY = "09"
+
+fun main() {
+    fun extrapolateNext(numbers: List<Long>): Long {
+        if (numbers.all { it == 0L }) return 0
+
+        val differences = numbers.windowed(2).map { (a, b) -> b - a }
+        return numbers.last() + extrapolateNext(differences)
+    }
+
+    fun part1(input: List<List<Long>>) =
+        input.sumOf { extrapolateNext(it) }
+
+    fun part2(input: List<List<Long>>) =
+        part1(input.map { it.reversed() })
+
+    val input = readInput("Day$DAY").map { it.toLongs() }
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun String.toLongs() = split(" ").map { it.toLong() }
