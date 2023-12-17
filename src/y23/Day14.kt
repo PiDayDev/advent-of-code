b15d78a083fc521919fb6739b0d3e238846fcdbd
Damiano Salvi <salvi@intre.it>
2023-12-17 16:27:43 +0100

    ✨ 2023 🎅 Refactor ♻️

--- src/y23/Day14.kt
+++ src/y23/Day14.kt
@@ -2,43 +2,42 @@ package y23
 
 private const val DAY = "14"
 
-data class RockyDish(
+data class Platform(
     val height: Int,
     val width: Int,
     val round: Set<Position>,
     val square: Set<Position>
 ) {
-    fun moveTo(direction: Position): RockyDish {
-        val yIndices = 0 until height
-        val xIndices = 0 until width
+    fun moveTo(direction: Position): Platform {
         var prev = round
         var curr = round
 
         fun isValid(candidate: Position) =
-            candidate !in prev && candidate !in square &&
-                    candidate.x in xIndices && candidate.y in yIndices
+            candidate.x in 0 until width &&
+            candidate.y in 0 until height &&
+            candidate !in square &&
+            candidate !in prev
 
         do {
             prev = curr
-            curr = prev.map { pos ->
-                generateSequence(pos + direction) { it + direction }
-                    .takeWhile { isValid(it) }.lastOrNull() ?: pos
-            }.toSet()
+            curr = prev
+                .map { pos -> (pos + direction).takeIf(::isValid) ?: pos }
+                .toSet()
         } while (prev != curr)
         return copy(round = curr)
     }
 
-    fun cycle(): RockyDish =
+    fun cycle(): Platform =
         moveTo(north).moveTo(west).moveTo(south).moveTo(east)
 
     fun totalLoad() = round.sumOf { height - it.y }
 }
 
-fun RockyDish(input: List<String>): RockyDish {
+fun Platform(input: List<String>): Platform {
     val height = input.size
     val width = input.first().length
-    val round = mutableListOf<Position>()
-    val square = mutableListOf<Position>()
+    val round = mutableSetOf<Position>()
+    val square = mutableSetOf<Position>()
     input.forEachIndexed { y, row ->
         row.forEachIndexed { x, c ->
             val pos = Position(x, y)
@@ -48,36 +47,36 @@ fun RockyDish(input: List<String>): RockyDish {
             }
         }
     }
-    return RockyDish(height, width, round.toSet(), square.toSet())
+    return Platform(height, width, round, square)
 }
 
 
 fun main() {
 
     fun part1(input: List<String>): Int =
-        RockyDish(input).moveTo(north).totalLoad()
+        Platform(input).moveTo(north).totalLoad()
 
     fun part2(input: List<String>): Int {
-        val cache = mutableListOf<RockyDish>()
+        val cache = mutableListOf<Platform>()
 
-        var currDish = RockyDish(input)
-        val last = 1000000000
+        var currPlatform = Platform(input)
+        val last = 1_000_000_000
         var index = 0
         while (index < last) {
-            index++
-            val nextDish = currDish.cycle()
-            if (nextDish in cache) {
-                val previousIndex = cache.lastIndexOf(nextDish)
-                val period = index - previousIndex - 1
+            val nextPlatform = currPlatform.cycle()
+            if (nextPlatform in cache) {
+                val previousIndex = cache.lastIndexOf(nextPlatform)
+                val period = index - previousIndex
                 val repeat = (last - index) / period
                 if (repeat > 0) {
                     index += repeat * period
                 }
             }
-            cache.add(nextDish)
-            currDish = nextDish
+            cache.add(nextPlatform)
+            currPlatform = nextPlatform
+            index++
         }
-        return currDish.totalLoad()
+        return currPlatform.totalLoad()
     }
 
     val input = readInput("Day$DAY")
