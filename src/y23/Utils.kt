6998f1cdf9a41f2bae47f4c131cb6dc1100b19e8
Damiano Salvi <salvi@intre.it>
2023-12-17 14:58:37 +0100

    ✨ 2023 🎅 Refactor ♻️

--- src/y23/Utils.kt
+++ src/y23/Utils.kt
@@ -1,57 +1,31 @@
 package y23
 
-import common.Point2D
-import common.x
-import common.y
 import java.io.File
-import java.math.BigInteger
-import java.util.*
 
 /**
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = File("src/y23", "$name.txt").readLines()
 
-/*
-ADVANCED DATA STRUCTURES
- */
-interface PriorityQ<T : Comparable<T>?> {
-    fun add(element: T): Boolean
-    fun updatePriority(oldElement: T, newElement: T): Boolean
-    fun remove(element: T): Boolean
-    fun top(): T?
-    operator fun contains(element: T): Boolean
-    val size: Int
-    fun isEmpty(): Boolean
-    fun isNotEmpty() = !isEmpty()
+data class Position(val x: Int, val y: Int) {
+    operator fun plus(p: Position) =
+        Position(x + p.x, y + p.y)
+
+    fun around() =
+        listOf(north, south, east, west).map { dir -> this + dir }
+
+    override fun toString() =
+        "($x,$y)"
 }
 
-class MyQ<T : Comparable<T>?>(private val queue: PriorityQueue<T> = PriorityQueue<T>()) :
-    PriorityQ<T>, Queue<T> by queue {
+val north = Position(x = 0, y = -1)
+val south = Position(x = 0, y = +1)
+val east = Position(x = 1, y = 0)
+val west = Position(x = -1, y = 0)
 
-    override fun updatePriority(oldElement: T, newElement: T): Boolean {
-        val wasPresent = queue.removeIf { it == oldElement }
-        if (wasPresent) queue.add(newElement)
-        return wasPresent
-    }
-
-    override fun top(): T? = poll()
-
+enum class Direction(val movement: Position) {
+    N(north),
+    S(south),
+    W(west),
+    E(east)
 }
-
-
-enum class Direction(val dx: Int, val dy: Int) {
-    UP(0, -1),
-    DOWN(0, 1),
-    LEFT(-1, 0),
-    RIGHT(1, 0);
-}
-
-operator fun Point2D.plus(d: Direction): Point2D = x + d.dx to y + d.dy
-
-
-val Int.b: BigInteger
-    get() = toBigInteger()
-
-val Long.b: BigInteger
-    get() = toBigInteger()
