875b421a3d7ff79eb7e7a8c08c284d65413cc035
Damiano Salvi <salvi@intre.it>
2023-12-18 18:07:10 +0100

    ✨ 2023 🎅 Day 18 / Part 2

--- src/y23/Day18.kt
+++ src/y23/Day18.kt
@@ -4,84 +4,64 @@ import y23.Direction.E
 import y23.Direction.N
 import y23.Direction.S
 import y23.Direction.W
-import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 private const val DAY = "18"
 
-data class DigInstruction(val direction: Direction, val length: Int, val color: String)
+data class DigInstruction(val direction: Direction, val length: Int)
 
 val stringToDirection = mapOf("U" to N, "D" to S, "L" to W, "R" to E)
 
-fun String.toDirection(): Direction = stringToDirection[this] ?: N
+fun String.toDigInstruction(): DigInstruction =
+    split(" ").let { (d, n) -> DigInstruction(stringToDirection[d] ?: N, n.toInt()) }
 
-fun String.toDigInstruction(): DigInstruction {
-    val (d, n, c) = split(" ")
-    return DigInstruction(d.toDirection(), n.toInt(), c.removeSurrounding("(", ")"))
+val idToDirection = mapOf('0' to E, '1' to S, '2' to W, '3' to N)
+
+fun String.toDigInstructionPart2(): DigInstruction {
+    val encoded = substringAfter("(#").substringBefore(")")
+    val direction = idToDirection[encoded.last()] ?: N
+    val length = encoded.take(5).toInt(16)
+    return DigInstruction(direction, length)
 }
 
-operator fun Position.plus(instruction: DigInstruction): List<Position> =
-    (1..instruction.length).map { k ->
-        this + instruction.direction.movement * k
+fun area(instructions: List<DigInstruction>): Long {
+    val curvatures = (instructions.takeLast(1) + instructions + instructions.take(1))
+        .windowed(3)
+        .map { (a, b, c) -> (a.direction to b.direction).turn() + (b.direction to c.direction).turn() }
+        .map { it.sign }
+    val globalCurvature = curvatures.sum().sign
+
+    val positions = mutableListOf(Position(0, 0))
+    instructions.zip(curvatures).forEach { (dig, curvature) ->
+        val convexityAdjustment = curvature * globalCurvature
+        val move = dig.direction.movement * (dig.length + convexityAdjustment)
+        positions.add(positions.last() + move)
     }
 
-fun Set<Position>.floodFill(): Set<Position> {
-    val xIndices = minOf { it.x }..maxOf { it.x }
-    val yIndices = minOf { it.y }..maxOf { it.y }
-
-    val startSearch = Position(xIndices.first - 1, (yIndices.first + yIndices.last) / 2)
-    val startPosition = generateSequence(startSearch) { it + E.movement }
-        .dropWhile { !contains(it) }
-        .first { !contains(it) }
-
-    val fill = mutableSetOf<Position>()
-
-    val queue = Stack<Position>()
-    queue.push(startPosition)
-    while (queue.isNotEmpty()) {
-        val next = queue.pop()
-        if (next in this || next in fill) continue
-
-        fill += next
-        queue += next.around()
-    }
-    return fill
+    return positions.shoelaceArea()
 }
+
+private fun List<Position>.shoelaceArea() = windowed(2)
+    .sumOf { (a, b) -> a.x.toLong() * b.y.toLong() - a.y.toLong() * b.x.toLong() }
+    .absoluteValue / 2
 
 fun main() {
-    fun digTrench(instructions: List<DigInstruction>): Set<Position> {
-        var currentPosition = Position(0, 0)
-        val trench = mutableSetOf(currentPosition)
-
-        instructions.forEach { instruction ->
-            val steps = currentPosition + instruction
-            trench += steps
-            currentPosition = steps.last()
-        }
-        return trench
-    }
-
-    fun part1(input: List<String>): Int {
-        val instructions = input.map(String::toDigInstruction)
-        val trench = digTrench(instructions)
-        val fill = trench.floodFill()
-        return (trench + fill).size
-    }
-
-    fun part2(input: List<String>): Int {
-        TODO()
-    }
-
-    // test if implementation meets criteria from the description, like:
-    try {
-        val testInput = readInput("Day${DAY}_test")
-        val p1 = part1(testInput)
-        println("Test 1 = $p1")
-        check(p1 == 62)
-    } catch (e: java.io.FileNotFoundException) {
-        // no tests
-    }
+    fun part1(input: List<String>) = area(input.map(String::toDigInstruction))
+    fun part2(input: List<String>) = area(input.map(String::toDigInstructionPart2))
 
     val input = readInput("Day$DAY")
-    println(part1(input))
-    println(part2(input))
+    println("PART 1 [SHOELACE] = ${part1(input)}")
+    println("PART 2 [SHOELACE] = ${part2(input)}")
+}
+
+/* 1 = clockwise, -1 = CCW */
+fun Pair<Direction, Direction>.turn(): Int {
+    val (a, b) = this
+    return when (a) {
+        N -> if (b == E) +1 else if (b == W) -1 else 0
+        S -> if (b == E) -1 else if (b == W) +1 else 0
+        W -> if (b == S) -1 else if (b == N) +1 else 0
+        E -> if (b == S) +1 else if (b == N) -1 else 0
+    }
 }
