7c4f6a6fde92e9af817efc32d87007388f05b40f
Damiano Salvi <salvi@intre.it>
2024-12-03 09:31:56 +0100

    ✨ 2024 🎉 Setup

---
+++ src/y24/Utils.kt
@@ -0,0 +1,34 @@
+package y24
+
+import java.io.File
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readInput(name: String) = File("src/y24", "$name.txt").readLines()
+
+data class Position(val x: Int, val y: Int) {
+    operator fun plus(p: Position) =
+        Position(x + p.x, y + p.y)
+
+    operator fun times(n: Int) =
+        Position(x * n, y * n)
+
+    fun around() =
+        listOf(north, south, east, west).map { dir -> this + dir }
+
+    override fun toString() =
+        "($x,$y)"
+}
+
+val north = Position(x = 0, y = -1)
+val south = Position(x = 0, y = +1)
+val east = Position(x = 1, y = 0)
+val west = Position(x = -1, y = 0)
+
+enum class Direction(val movement: Position) {
+    N(north),
+    S(south),
+    W(west),
+    E(east)
+}
