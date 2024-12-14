dc3ea493926b0e0e2c4336173b6b5a969803e1b2
Damiano Salvi <salvi@intre.it>
2024-12-14 16:18:22 +0100

    ✨ 2024 🎅 Day 14 ♻️ Refactor

--- src/y24/Day14.kt
+++ src/y24/Day14.kt
@@ -1,29 +1,27 @@
 package y24
 
-import java.io.File
-import java.io.FileWriter
-import java.io.PrintWriter
+import kotlin.math.hypot
+import kotlin.math.pow
 
 private const val DAY = "14"
 
 typealias Velocity = Position
 
-data class Quadrant(
-    val xRange: IntRange,
-    val yRange: IntRange,
-) {
+data class Quadrant(val xRange: IntRange, val yRange: IntRange) {
+
     operator fun contains(position: Position) =
         position.x in xRange && position.y in yRange
 
 }
 
+infix fun List<Position>.countIn(quadrant: Quadrant): Int = count { it in quadrant }
+
 data class Grid(val width: Int, val height: Int) {
-    fun constrain(newRobot: Robot): Position {
-        val x = newRobot.position.x
-        val y = newRobot.position.y
-        val newX = (x % width + width) % width
-        val newY = (y % height + height) % height
-        return Position(newX, newY)
+
+    fun constrain(position: Position): Position {
+        val x = (position.x % width + width) % width
+        val y = (position.y % height + height) % height
+        return Position(x, y)
     }
 
     fun quadrants(): List<Quadrant> {
@@ -38,87 +36,82 @@ data class Grid(val width: Int, val height: Int) {
             Quadrant(x3..x4, y3..y4),
         )
     }
+
+    fun safetyFactor(positions: List<Position>): Int = quadrants()
+        .map { positions countIn it }
+        .reduce { a, b -> a * b }
+
 }
+
 
 data class Robot(val position: Position, val velocity: Velocity) {
 
-    fun move(): Robot {
-        val newPosition = position + velocity
-        return copy(position = newPosition)
-    }
-
+    fun move(): Robot = copy(position = position + velocity)
     fun moveInside(grid: Grid): Robot {
-        val newRobot = this.move()
-        return copy(position = grid.constrain(newRobot))
+        val newRobot = move()
+        return copy(position = grid.constrain(newRobot.position))
     }
 }
 
 fun String.toRobot(): Robot {
     val p = substringAfter("p=").substringBefore(" ")
     val v = substringAfter("v=")
-    val (px, py) = p.split(",").map { it.toInt() }
-    val (vx, vy) = v.split(",").map { it.toInt() }
-    return Robot(
-        Position(px, py),
-        Velocity(vx, vy)
-    )
+
+    fun String.toInts() = split(",").map { it.toInt() }
+    val (px, py) = p.toInts()
+    val (vx, vy) = v.toInts()
+
+    return Robot(Position(px, py), Velocity(vx, vy))
 }
 
-fun parseAndMove100Times(input: List<String>, grid: Grid): List<Position> =
-    (1..100)
+
+fun safetyFactor(input: List<String>, grid: Grid): Int {
+    val finalPositions = (1..100)
         .fold(input.map { it.toRobot() }) { robots, _ ->
             robots.map { it.moveInside(grid) }
         }
         .map { it.position }
-
-
-infix fun List<Position>.countIn(quadrant: Quadrant): Int = count { it in quadrant }
-
-fun safetyFactor(input: List<String>, grid: Grid): Int {
-    val finalPositions = parseAndMove100Times(input, grid)
-    val quadrants = grid.quadrants()
-
-    val counts = quadrants.map { finalPositions countIn it }
-
-    return counts.reduce { a, b -> a * b }
+    return grid.safetyFactor(finalPositions)
 }
 
-fun dispersion(positions: Set<Position>): Double =
-    positions.sumOf { it.around().count { p -> p in positions } }/(4.0*positions.size)
-
 fun main() {
-    fun part1(input: List<String>): Int {
-        val grid = Grid(101, 103)
-        return safetyFactor(input, grid)
-    }
+    val mainGrid = Grid(101, 103)
 
-    fun PrintWriter.printRobots(robots: List<Robot>, grid: Grid) {
+    fun part1(input: List<String>) = safetyFactor(input, mainGrid)
+
+    fun printRobots(robots: List<Robot>, grid: Grid) {
         val positions = robots.map { it.position }.toSet()
         (0 until grid.height).forEach { y ->
-            (0 until grid.width).forEach { x ->
-                val position = Position(x, y)
-                print(if (position in positions) '*' else '.')
+            val string = (0 until grid.width).joinToString("") { x ->
+                when (Position(x, y)) {
+                    in positions -> "█"
+                    else -> "."
+                }
             }
-            println()
+            println(string)
         }
     }
 
     fun part2(input: List<String>): Int {
-        val file = File("Day14.tree.txt")
-        val writer = PrintWriter(FileWriter(file))
-        var robots = input.map { it.toRobot() }
-        val grid = Grid(101, 103)
-        val result = (1..10000).map { s ->
-            robots = robots.map { it.moveInside(grid) }
-            s to dispersion(robots.map { it.position }.toSet())
+        val initialRobots = input.map { it.toRobot() }
+        val maxTries = mainGrid.width * mainGrid.height
+
+        fun Set<Position>.variance(): Double {
+            val avgX = map { it.x }.average()
+            val avgY = map { it.y }.average()
+            return map { hypot(avgX - it.x, avgY - it.y).pow(2) }.average()
         }
-        result.sortedByDescending { it.second }
-            .forEach(::println)
 
-        return 0
+        val (bestTime, bestRobots) = generateSequence(0 to initialRobots) { (time, robots) ->
+            time + 1 to robots.map { it.moveInside(mainGrid) }
+        }
+            .take(maxTries)
+            .minBy { (_, bots) -> bots.map { it.position }.toSet().variance() }
+
+        printRobots(bestRobots, mainGrid)
+
+        return bestTime
     }
-
-    // test if implementation meets criteria from the description, like:
 
     val input = readInput("Day$DAY")
     println(part1(input))
