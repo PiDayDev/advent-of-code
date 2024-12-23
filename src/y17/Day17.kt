b9383d62167fa1bb2b05064349c6decfcdfded68
Damiano Salvi <salvi@intre.it>
2024-12-24 00:46:06 +0100

    ⚡🎅 Performance

--- src/y17/Day17.kt
+++ src/y17/Day17.kt
@@ -2,22 +2,39 @@ package y17
 
 private const val steps = 363
 
+private class Node(val item: Int) {
+    var next: Node = this
+    fun append(newNode: Node) {
+        newNode.next = next
+        next = newNode
+    }
+}
+
 fun main() {
+
     fun shortCircuit(steps: Int, loopCount: Int, findAfter: Int): Int {
-        val successors = HashMap<Int, Int>(loopCount + 1)
-        successors[0] = 0
-        (1..loopCount).forEach {
-            val prev = it - 1
-            val src = (1..steps % successors.size).fold(prev) { n, _ -> successors[n]!! }
-            val dst = successors[src]!!
-            successors[src] = it
-            successors[it] = dst
+        var node = Node(0)
+        var t = System.currentTimeMillis()
+        for (currentSize in 1..loopCount) {
+            repeat(steps) {
+                node = node.next
+            }
+            Node(currentSize).let { new ->
+                node.append(new)
+                node = new
+            }
+            if (currentSize % 1_000_000 == 0) {
+                println("${System.currentTimeMillis() - t}ms #### Loop $currentSize of $loopCount")
+                t = System.currentTimeMillis()
+            }
         }
-        return successors[findAfter]!!
+        val goal = generateSequence(node) { it.next }.first { it.item == findAfter }
+
+        return goal.next.item
     }
 
     println(shortCircuit(steps = steps, loopCount = 2017, findAfter = 2017))
 
-    // warning: EXTREMELY SLOW
+    // warning: STILL EXTREMELY SLOW
     println(shortCircuit(steps = steps, loopCount = 50_000_000, findAfter = 0))
 }
